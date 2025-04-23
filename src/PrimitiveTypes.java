4577d559c5114e779ba3c801fda39b84092cb727
PHDumont <phdumont06@gmail.com>
2025-04-23 17:16:06 -0300

    feat: PrimitivesTypes and exercises

---
+++ src/PrimitiveTypes.java
@@ -0,0 +1,17 @@
+public class PrimitiveTypes {
+    public static void main(String[] args) {
+        int age = 10;
+        long bigNumber = 10000;
+        float salaryFloat = 2500.0F;
+        double salaryDouble = 2500;
+        boolean  trueValue = true;
+        short ageShort = 29;
+        byte ageByte = 39;
+        char character = 'a';
+        String name = "ada";
+        System.out.println("Age: " + age + " years old");
+        System.out.println(character);
+        System.out.println("I am " + name);
+    }
+}
+
---
+++ src/PrimitiveTypesExercise.java
@@ -0,0 +1,12 @@
+public class PrimitiveTypesExercise {
+    public static void main(String[] args) {
+        var name = "Peter";
+        var address = "Brazil";
+        var salary = 2000;
+        var date = "01/04";
+        var report = "I " + name + ", living on " + address + ", confirm that I received the salary of " + salary + " on " + date
+
+        System.out.println(report);
+
+    }
+}
