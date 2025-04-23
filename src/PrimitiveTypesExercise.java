5ac78501d366ed5d916b7c271f731be7568dccfb
PHDumont <phdumont06@gmail.com>
2025-04-23 17:28:00 -0300

    Fix: print in PrimitiveTypesExercise didn't work

--- src/PrimitiveTypesExercise.java
+++ src/PrimitiveTypesExercise.java
@@ -4,7 +4,7 @@ public class PrimitiveTypesExercise {
         var address = "Brazil";
         var salary = 2000;
         var date = "01/04";
-        var report = "I " + name + ", living on " + address + ", confirm that I received the salary of " + salary + " on " + date
+        var report = "I " + name + ", living on " + address + ", confirm that I received the salary of " + salary + " on " + date)
 
         System.out.println(report);
 
