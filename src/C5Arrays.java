08a573a29f601c2372190c4976df67c3a6582fe5
PHDumont <phdumont06@gmail.com>
2025-04-27 12:33:53 -0300

    feat: Arrays

---
+++ src/C5Arrays.java
@@ -0,0 +1,16 @@
+public class C5Arrays {
+    public static void main(String[] args) {
+        //byte, short, double, float, long, int = 0
+        // char '\u0000' ''
+        // boolean false
+        // String null
+        String[] names = new String[3];
+        names[0] = "Alberto";
+        names[1] = "Bernard";
+        names[2] = "Caio";
+
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+    }
+}
